37834718c63964e1b5b2e53f82b3772c71a1f0ef
red apple <63294124+muenzhang@users.noreply.github.com>
2020-06-09 10:44:04 +0800

    1.3

--- list.c
+++ list.c
@@ -4,27 +4,29 @@
 typedef struct node{
     int val;
     struct node * next;
+    int len;
 }list;
 
 list* listCreate(){
 	list *a = (list*)malloc(sizeof(list));
 	a -> next = NULL;
 	a -> val = -1;
+	a -> len = 0;
 	list *head = a;
+	
 	return head;
 }
 
 int len(list* head){
-	list *p = head -> next;
-	int count = 1;
-	while (p -> next && head){
-		p = p -> next;
-		count++;
-	}
-	return count;
+	return head -> len;
 }
 
 list* get(list *head,int number){
+	if (head -> len <= number){
+		printf("IndexError: list index out of range len(list) = %d,number = %d\n",len(head),number);
+		exit(-1);
+		return NULL;
+	}
     list *p = head -> next;
     for(int i = 0;i < number && p -> next;i++){
         p = p -> next;
@@ -41,6 +43,7 @@ list* append(list *head,int val){
     	a -> val = val;
     	a -> next = NULL;
 		head -> next = a;
+		head -> len += 1;
 		return head;
 	}
     
@@ -53,6 +56,8 @@ list* append(list *head,int val){
     a -> next = NULL;
     
     p -> next = a;
+    
+    head -> len += 1;
     
 	return head;
     	
@@ -69,7 +74,11 @@ int search(list* head,int j){
 }
 
 list* insert(list *head,int i,int val){
-	
+	if (head -> len < i){
+		printf("IndexError: list index out of range len(list) = %d,number = %d\n",len(head),i);
+		exit(-1);
+		return NULL;
+	}
 	
     if (i == 0){
     	if (!head -> next){
@@ -77,6 +86,7 @@ list* insert(list *head,int i,int val){
 			a -> val = val;
 			a -> next = NULL;
 			head -> next = a;
+			head -> len += 1;
 			return head;
 		}
 
@@ -85,6 +95,7 @@ list* insert(list *head,int i,int val){
         a -> next = head -> next -> next;
         head -> next -> next = a;
         head ->  next -> val = val;
+        head -> len += 1;
     }
     
     if (i != 0){
@@ -95,6 +106,7 @@ list* insert(list *head,int i,int val){
         a -> val = val;
         a -> next = p -> next;
         p -> next = a;
+        head -> len += 1;
 
 
     }
@@ -102,16 +114,24 @@ list* insert(list *head,int i,int val){
 
 int del(list *head,int i){
 	
+	if (len(head) <= i){
+		printf("IndexError: list assignment index out of range.len(list) = %d,number = %d\n",len(head),i);
+		exit(-1);
+		return -1;
+	}
+	
 	if (i == 0){
 		if (!head -> next -> next){
 			list *p = head -> next;
 			head -> next = NULL;
+			head -> len -= 1;
 			free(p);
 			return 0;
 		}
 		if (!head -> next -> next -> next){
 			list *p = head -> next;
 			head -> next = head -> next -> next;
+			head -> len -= 1;
 			free(p);
 			return 0;
 		}
@@ -119,6 +139,7 @@ int del(list *head,int i){
   		head -> next -> val = head -> next -> next -> val;
   		list *p = head -> next -> next;
 		head -> next -> next = head -> next -> next -> next;
+		head -> len -= 1;
 		free(p);
 	}
     if (i != 0){
@@ -128,6 +149,7 @@ int del(list *head,int i){
 
 		list *p2 = p -> next;
         p -> next = p -> next -> next;
+        head -> len -= 1;
         free(p2);
     }
 }
